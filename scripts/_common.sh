b551d4af29c9b7cf745b8610fec6f3da645e73eb
ericgaspar <junk.eg@free.fr>
2022-08-14 20:37:32 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,6 @@
 # PHP APP SPECIFIC
 #=================================================
 
-# Composer version
-YNH_COMPOSER_VERSION="2.3.10"
-
 pkg_dependencies="postgresql php$YNH_DEFAULT_PHP_VERSION-cli php$YNH_DEFAULT_PHP_VERSION-intl php-php-gettext php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-pgsql"
 
 #php$YNH_PHP_VERSION-pcntl 
