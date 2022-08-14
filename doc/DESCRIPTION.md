f27653e55f99a3a946babeb4f7d9122dacb6af57
ericgaspar <junk.eg@free.fr>
2022-08-14 10:43:20 +0200

    First commit

---
+++ doc/DESCRIPTION.md
@@ -0,0 +1,8 @@
+flusio brings together news feed aggregation and social bookmarking in a modern way. It is designed as a simple, yet complete tool for organising the links you gather around the Web.
+
+### Features
+
+- the feeds aggregation (RSS and Atom) to follow any website, podcast or video channel in one place;
+- the bookmarks and collections to save your favourites articles for later and to organise them;
+- the news to keep control over your newsfeed;
+- the profile to share links with others.
---
+++ scripts/_common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+# PHP APP SPECIFIC
+#=================================================
+
+pkg_dependencies="postgresql php$YNH_DEFAULT_PHP_VERSION-cli php$YNH_DEFAULT_PHP_VERSION-intl php-php-gettext php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-pgsql"
+
+#php$YNH_PHP_VERSION-pcntl 
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
